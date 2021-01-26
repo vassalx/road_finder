2cdf33767f8c9b0bcd7cfa22565b3902350d3a73
VassalX <vasyacat@protonmail.com>
2021-01-26 23:58:46 +0200

    split to test and train

--- build_dataset.py
+++ build_dataset.py
@@ -7,6 +7,35 @@ import cv2
 from tqdm import tqdm
 import os
 
+def get_filenames(files_path):
+    return [os.path.basename(x) for x in glob.glob(files_path)]
+
+def split_to_test_and_train(root_path, images_path, masks_path, test_split=0.3):
+    image_filenames = get_filenames(images_path +"*.tiff")
+    test_set_size = int(test_split * len(image_filenames))
+    
+    train_dir = root_path + "Train/"
+    test_dir = root_path + "Test/"
+
+    train_image_dir = train_dir+"Images/"
+    train_mask_dir = train_dir+"Masks/"
+    test_image_dir = test_dir+"Images/"
+    test_mask_dir = test_dir+"Masks/"
+    
+    for i in range(test_set_size):
+        filename = image_filenames[i]
+        os.renames(images_path + filename, test_image_dir + filename)
+        os.renames(masks_path + filename, test_mask_dir + filename)
+    
+    for i in range(test_set_size, len(image_filenames)):
+        filename = image_filenames[i]
+        os.renames(images_path + filename, train_image_dir + filename)
+        os.renames(masks_path + filename, train_mask_dir + filename)
+            
+    print("Train-Test-Split COMPLETED.\nNUMBER OF IMAGES IN TRAIN SET:{}\nNUMBER OF IMAGES IN TEST SET: {}".format(len(image_filenames)-test_set_size, test_set_size))
+    print("\nTrain Directory:", train_dir)
+    print("Test Directory:", test_dir)
+
 def crop_and_save(images_path, masks_path, result_images_path, result_masks_path, img_size):
     print("Dataset Build Started!")
     
@@ -14,7 +43,7 @@ def crop_and_save(images_path, masks_path, result_images_path, result_masks_path
 
     print(images_path)
     
-    image_files = [os.path.basename(x) for x in glob.glob(images_path + "*.tiff")]
+    image_files = get_filenames(images_path +"*.tiff")
     print('Number of files: {}'.format(len(image_files)))
     
     start_time = time.time()
@@ -27,7 +56,6 @@ def crop_and_save(images_path, masks_path, result_images_path, result_masks_path
         mask_path = masks_path + image_filename[:-1]
         mask = cv2.imread(mask_path, cv2.IMREAD_GRAYSCALE)
         
-        # num_splits = math.floor((image.shape[0]*image.shape[1])/(img_size*img_size))
         counter = 0
         
         for r in range(0, image.shape[0], img_size):
@@ -36,31 +64,36 @@ def crop_and_save(images_path, masks_path, result_images_path, result_masks_path
                 
                 cropped_image, cropped_mask = crop(image, mask, r, r + img_size, c, c + img_size)
                 
-                blank_mask[blank_mask>1] = 255
+                cropped_mask[cropped_mask>1] = 255
                 
-                if np.any(blank_mask):
-                    num_black_pixels, num_white_pixels = np.unique(blank_mask, return_counts=True)[1]
+                if np.any(cropped_mask):
+                    black_mask_num, white_mask_num = np.unique(cropped_mask, return_counts=True)[1]
                     
-                    if num_white_pixels/num_black_pixels < 0.01:
+                    if white_mask_num/black_mask_num < 0.01:
                         skipped_num += 1
                         continue
 
-                    cv2.imwrite(result_images_path + str(counter) + '_' + image_filename, blank_image)
-                    cv2.imwrite(result_masks_path + str(counter) + '_' + image_filename, blank_mask)
-                
-    
+                    white_image_num = np.sum(cropped_image == 255)
+
+                    if white_image_num/cropped_image.size > 0.1:
+                        skipped_num += 1
+                        continue
+
+                    cv2.imwrite(result_images_path + str(counter) + '_' + image_filename, cropped_image)
+                    cv2.imwrite(result_masks_path + str(counter) + '_' + image_filename, cropped_mask)
+
     print("complete: {} seconds.\images in {}\masks in{}".format(round((time.time()-start_time), 2), result_images_path, result_masks_path))
     print("\nNumber of skipped images: {}".format(skipped_num))
 
 def crop(image, mask, r1, r2, c1, c2):
     result_image = np.zeros((img_size , img_size, 3), np.uint8)
     result_mask = np.zeros((img_size , img_size), np.uint8)
-                
+
     cropped_image = image[r1:r2, c1:c2]
-    cropped_mask = mask[r1:r2, c:c2]
-                
-    result_image[:cropped_image.shape[0], :cropped_image.shape[1]] += new_image
-    result_mask[:cropped_mask.shape[0], :cropped_mask.shape[1]] += new_mask
+    cropped_mask = mask[r1:r2, c1:c2]
+
+    result_image[:cropped_image.shape[0], :cropped_image.shape[1]] += cropped_image
+    result_mask[:cropped_mask.shape[0], :cropped_mask.shape[1]] += cropped_mask
 
     return(result_image, result_mask)
 
@@ -85,4 +118,4 @@ if __name__ == "__main__":
         os.mkdir(result_masks_path)
 
     crop_and_save(images_path, masks_path, result_images_path, result_masks_path, img_size)
-    train_test_split(result_images_path, result_masks_path, test_files_percent)
+    split_to_test_and_train(root, result_images_path, result_masks_path, test_files_percent)
